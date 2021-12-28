fc639b8f4338776d19df5a271c84ab4f07c2f814
James Barry <jbm03003@gmail.com>
2021-12-28 15:52:54 -0500

    added fetch call to openweather api

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
 var apiKey = "b7df24583d533f6d77955dd6376b065c";
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" + apiKey;
+
+fetch(apiUrl)
+    .then(function(serverPromise){ 
+      serverPromise.json()
+        .then(function(j) { 
+          console.log(j); 
+        })
+        .catch(function(e){
+          console.log(e);
+        });
+    })
+    .catch(function(e){
+        console.log(e);
+      });
